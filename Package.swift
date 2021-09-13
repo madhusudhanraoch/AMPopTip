2a8241b650dc44ab601bc4f121b15484dce57b2c
madhusudhanraoch <madhufr.you@gmail.com>
2021-09-13 16:21:41 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,21 +1,21 @@
 // swift-tools-version:5.0
 
+
 import PackageDescription
 
 let package = Package(
-  name: "AMPopTip",
-    platforms: [
-        .iOS(.v12),
+    name: "AMPopTip",
+    platforms: [.iOS(.v12), .macOS(.v10_14), .tvOS(.v12), .watchOS(.v5)],
+    products: [
+        .library(name: "AMPopTip", targets: ["AMPopTip"])
     ],
-  products: [
-    .library(
-      name: "AMPopTip",
-      targets: ["AMPopTip"])
-  ],
-  targets: [
-    .target(
-      name: "AMPopTip",
-      path: "Source")
-  ]
+    targets: [
+        .target(
+            name: "AMPopTip",
+            path: "Sources"
+        )
+    ]
 )
+
+
 
