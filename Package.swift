0b4ec05c3b95276fcd0e4f81d0dd0cde23e0d4c6
madhusudhanraoch <madhufr.you@gmail.com>
2021-09-13 11:50:22 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,20 +1,31 @@
 // swift-tools-version:5.0
 
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
-  name: "AMPopTip",
-   platforms: [
+    name: "AMPopTip",
+    platforms: [
         .iOS(.v12),
     ],
-  products: [
-    .library(
-      name: "AMPopTip",
-      targets: ["AMPopTip"])
-  ],
-  targets: [
-    .target(
-      name: "AMPopTip",
-      path: "Source")
-  ]
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "AMPopTip",
+            targets: ["AMPopTip"])
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "AMPopTip",
+            dependencies: [],
+            path: "Source"
+        )
+    ]
 )
